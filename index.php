d0287bf2a3ed028b92c12f0b35b9a2a9d90410bf
Om12ek99 <omar.elkamili@gmail.com>
2024-05-20 17:42:05 +0200

    add:added file analyzer.php and form section

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bad Word Ban</title>
+</head>
+
+<body>
+    <section>
+        <!-- form -->
+        <form action="analyzer.php" method="GET">
+            <input type="text" name="parag"> 
+            <input type="text" name="banned">  
+            <button type="submit">Censura</button> 
+        </form>
+    </section>
+</body>
+</html>
